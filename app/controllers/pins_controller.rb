48000be84bbc686f23589b453059f8a727e338ba
Becky Bair <beckyhowen@gmail.com>
2015-12-16 21:05:24 -0600

    Adding named route for show_by_name route

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -8,4 +8,10 @@ class PinsController < ApplicationController
     @pin = Pin.find(params[:id])
   end
   
+  def show_by_name
+    #search for a Pin using the slug you grab from the URL
+    @pin = Pin.find_by_slug(params[:slug])
+    render :show
+  end
+  
 end
