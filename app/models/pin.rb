7b0364201435b0c4f2914f82fb199fa8dda337ea
Becky Bair <beckyhowen@gmail.com>
2016-01-09 03:55:05 -0600

    changes to add images - doesn't upload

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,9 @@ class Pin < ActiveRecord::Base
   validates_uniqueness_of :slug
   
   belongs_to :category
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "60x60>" }, default_url: "http://placebear.com/60/60"
+
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  
 end
